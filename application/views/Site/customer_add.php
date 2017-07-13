f93b4e7f0bf8c32d21e910248f1c62494ccaf308
Radek <firtus911@wp.pl>
2017-07-13 10:03:59 +0200

    css work

--- application/views/Site/customer_add.php
+++ application/views/Site/customer_add.php
@@ -1,6 +1,6 @@
 <html>
 <link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+//<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -10,62 +10,46 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="css/bootstrap.css">
 
-<!-- <body> -->
+<body>
 
-<!-- <div class="row margin-top"> -->
-<!-- <center>	 -->
-<!-- 		<div class="panel"> -->
-<!--         	<form metod="post"> -->
 
-<!--               		<label for="username">Imie</label></br> -->
-<!--                    		<input type="text" id="Name" name="Name"></br> -->
-<!--                    	<label for="username">Nazwisko</label></br> -->
-<!--                    		<input type="text" id="Surname" name="Surname"></br> -->
-<!--     				<label for="firma">Państwo</label></br> -->
-<!--                    		<input type="text" id="Country" name="Country"></br> -->
-<!--     				<label for="firma">Kod Pocztowy</label></br> -->
-<!--                    		<input type="text" id="PostalCode" name="PostalCode"></br> -->
-<!--                    	<label for="firma">Adres</label></br> -->
-<!--                    		<input type="text" id="Street" name="Street"></br> -->
-<!--                    	<label for="firma">Numer bloku</label></br> -->
-<!--                    		<input type="text" id="HouseNo" name="HouseNo"></br> -->
-<!--                    	<label for="firma">Numer drzwi</label></br> -->
-<!--                    		<input type="text" id="ApartmentNo" name="ApartmetNo"></br> -->
-<!--                    	<label for="firma">NIP</label></br> -->
-<!--                    		<input type="text" id="NIP" name="NIP"></br> -->
-<!--  </center>  -->
+<div class="row marginess-top">
+<center>	
+		<div class="panel">
+        	<form metod="post">
+
+              		<label for="username">Imie</label></br>
+                   		<input type="text" id="Name" name="Name"></br>
+                   	<label for="surname">Nazwisko</label></br>
+                   		<input type="text" id="Surname" name="Surname"></br>
+    				<label for="country">Państwo</label></br>
+                   		<input type="text" id="Country" name="Country"></br>
+    				<label for="postalcode">Kod Pocztowy</label></br>
+                   		<input type="text" id="PostalCode" name="PostalCode"></br>
+                   	<label for="adress">Adres</label></br>
+                   		<input type="text" id="Street" name="Street"></br>
+                   	<label for="houseno">Numer bloku</label></br>
+                   		<input type="text" id="HouseNo" name="HouseNo"></br>
+                   	<label for="apartmenno">Numer drzwi</label></br>
+                   		<input type="text" id="ApartmentNo" name="ApartmetNo"></br>
+                   	<label for="nip">NIP</label></br>
+                   		<input type="text" id="NIP" name="NIP"></br>
+ </center> 
           
-<!-- <div class="row"> -->
-<!-- 	<div class="col-sm-5"></div> -->
-<!-- 	<div class="col-sm-5"> -->
-<!--                 <div class="btn"> -->
-<!--             		<input type="submit" value="Dodaj"> -->
-<!--     			</div> -->
-<!--     			<div class="btn"> -->
-<!--             		<input type="reset" value="Wyczyść"> -->
-<!-- 				</div></div> -->
-<!-- 	<div class="col-sm-5"></div> -->
-<!-- 			</form> -->
-<!-- 		</div> -->
-<!-- </div> -->
-  
-  
-<div class="container">
-<div class="input-group input_group-lg"></div>
-<span class="input-group-addon">Imie</span>
-<input type="text" class="form-control" id="Name" name="Name" placeholder="Imie">
-<span class="input-group-addon">Nazwisko</span>
-<input type="text" class="form-control" id="Name" name="Name" placeholder="Nazwisko">
-<span class="input-group-addon">Panstwo</span>
-<input type="text" class="form-control" id="Name" name="Name" placeholder="Panstwo">
-<span class="input-group-addon">Kod Pocztowy</span>
-<input type="text" class="form-control" id="Name" name="Name" placeholder="Kod Pocztowy">
-<span class="input-group-btn">
-<button class="btn btn-default">Dodaj</button>
-</span>
-</div>  
-  
-  
+<div class="row">
+	<div class="col-sm-5"></div>
+	<div class="col-sm-2 col-2-margin">
+                <div class="btn">
+            		<input type="submit" value="Dodaj">
+    			</div>
+    			<div class="btn">
+            		<input type="reset" value="Wyczyść">
+				</div></div>
+	<div class="col-sm-5"></div>
+			</form>
+		</div>
+</div>
+
   
   
   
--- application/views/Site/customer_view.php
+++ application/views/Site/customer_view.php
@@ -1,12 +1,12 @@
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="style.css">
-<!--     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"> -->
-<!--     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 </head>
 <body>
 
-
+</style>
 
 <div class="container margines-top">
     <table class="table table-striped table-hover">
