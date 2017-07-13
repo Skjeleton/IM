97da7199cc674aac88cbbf8fe6b36fac8e840ac7
Radek <firtus911@wp.pl>
2017-07-13 09:37:13 +0200

    Bootstrap v4.0

--- application/views/Site/customer_view.php
+++ application/views/Site/customer_view.php
@@ -1,44 +1,37 @@
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="css/style.css">
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-   
-
-
-
+    <link rel="stylesheet" type="text/css" href="style.css">
+<!--     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"> -->
+<!--     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script> -->
 </head>
-
 <body>
 
-<div class="kupa">
+
+
+<div class="container margines-top">
+    <table class="table table-striped table-hover">
+        <thead>
+            <tr>
+                <th>Nazwa</th>
+                <th>Adres</th>
+                <th></th>
+            </tr>
+        </thead>
+            <tbody>
+            <?php foreach($customers as $customer){
+                echo "<tr>";    
+                
+                echo "<td>".$customer["Name"]."</td>";
+                echo "<td>".$customer["Address"]."</td>";
+                echo "<td><button>Edytuj</button>";
+                
+                echo "</tr>";
+            
+            }
+            ?>
+            
+      	  </tbody>
+    </able>
 </div>
-<div class="container">
-<table class="table table-striped table-hover">
-<thead>
-<tr>
-<th>Nazwa</th>
-<th>Adres</th>
-<th></th>
-</tr>
-</thead>
-<tbody>
-<?php foreach($customers as $customer){
-echo "<tr>";    
-
-echo "<td>".$customer["Name"]."</td>";
-echo "<td>".$customer["Address"]."</td>";
-echo "<td><button>Edytuj</button>";
-
-echo "</tr>";
-
-}
-?>
-
-</tbody>
-</table></div>
 </body>
 </html>
