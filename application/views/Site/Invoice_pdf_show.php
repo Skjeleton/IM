fa89989ce3744ec649e716943085350045e1e5bb
Radek <firtus911@wp.pl>
2017-07-14 10:53:20 +0200

    asd

---
+++ application/views/Site/Invoice_pdf_show.php
@@ -0,0 +1,21 @@
+<html>
+<head>	
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/style.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.css">
+</head>
+<body>
+	<div class="row">
+		<div class="col-lg-6"><div class="abastra"></div></div>
+		<div class="col-lg-6">
+    		 <div class="panel-group">
+             	<div class="panel panel-default">
+             		<div class="panel-heading">FAKTURA VAT</div>
+             		<div class="panel-body">
+<!--              			Content -->
+             		</div>
+    			</div>
+			</div>
+	</div>
+</body><
+</html>
